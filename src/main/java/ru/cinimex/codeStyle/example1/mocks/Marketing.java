e4a5a802ed59c39711aa20d114c6bc365a07ad2b
Yaroslav Berezin <yb2003work@gmail.com>
2024-05-24 15:09:32 +0300

    feat: initial commit

---
+++ src/main/java/ru/cinimex/codeStyle/example1/mocks/Marketing.java
@@ -0,0 +1,33 @@
+package ru.cinimex.codeStyle.example1.mocks;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class Marketing {
+
+    private Long id;
+    private Long clientId;
+    private Boolean isPaid;
+    private Integer orderId;
+    private LocalDateTime scoringDate;
+    private String ruleBlockNumber;
+    private Long decisionId;
+    private Boolean optionAvailability;
+    private Boolean showMode;
+    private Boolean isChosen;
+    private Boolean isFinalOption;
+    private Long scoringTypeId;
+    private Integer includeIntoPayment;
+    private Option option;
+
+
+    public Option getOption(){
+        return new Option();
+    }
+
+    public Marketing setIsFinalOption(Boolean isFinalOption){
+        return this.setIsFinalOption(isFinalOption);
+    }
+
+}
